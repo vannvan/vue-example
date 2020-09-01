b6d7cb2eb857d28fc398114b210539529d881a7b
vannvan <862670198@qq.com>
2020-09-01 15:15:19 +0800

    add ssr-example

---
+++ ssr-example/src/main.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import App from "./App.vue";
+// import router from "./router";
+// import store from "./store";
+import { createRouter } from "./router";
+
+Vue.config.productionTip = false;
+
+
+
+
+// 导出一个工厂函数，用于创建新的
+// 应用程序、router 和 store 实例
+export function createApp() {
+    const router = createRouter();
+    const app = new Vue({
+        router,
+        // 根实例简单的渲染应用程序组件。
+        render: h => h(App)
+    })
+    return { app, router }
+}
