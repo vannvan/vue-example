234b506c3a8ad4d87e348fcbcebf86ce0d06ff17
vannvan <862670198@qq.com>
2020-11-30 23:08:45 +0800

    auto-commit

---
+++ vue-next-demo-1/src/App.vue
@@ -0,0 +1,12 @@
+<script>
+import { ref, h } from 'vue'
+export default {
+  name: 'App',
+  components: {},
+  setup(props) {
+    console.log('app')
+    const title = ref('vue3')
+    return () => h('div', 'ahhaha')
+  },
+}
+</script>
---
+++ vue-next-demo-1/src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template></template>
+<script>
+import { h } from 'vue'
+export default {
+  name: 'HelloWorld',
+  setup(props) {
+    return h('div', 'hello world')
+    console.log(props)
+    return {}
+  },
+}
+</script>
